58d917df598cae2345365270d9e28150d47c7696
fgrims <fra.grim198@gmail.com>
2024-05-17 16:57:27 +0200

    project commit

---
+++ app/src/main/java/com/upside/accelerometerlogger/AccelerometerLoggingService.java
@@ -0,0 +1,106 @@
+package com.upside.accelerometerlogger;
+
+import android.app.Service;
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class AccelerometerLoggingService extends Service implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    public File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    public File file = new File(this.downloadsDir, "accelerometer_log.txt");
+    public FileWriter outfile;
+    public BufferedWriter bw;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if (accelerometer != null) {
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                System.out.println("Accelerometer sensor registered");
+            } else {
+                Log.e("AccelerometerLogging", "Accelerometer sensor not available");
+            }
+        }
+
+        try{
+            if (!this.file.exists()) {
+                this.file.createNewFile();
+            }
+            this.outfile = new FileWriter(this.file.getAbsoluteFile());
+            String[] header = { "accelerometer.x", "accelerometer.y", "accelerometer.z" };
+            this.bw = new BufferedWriter(this.outfile);
+            this.bw.write(Arrays.toString(header));
+
+        } catch (IOException e) {
+            Log.e("AccelerometerLogging", "Error opening file for writing");
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Start the service in the foreground if required
+        // This is necessary for the service to continue running in the background
+        // Return START_STICKY to ensure the service restarts if it's killed by the system
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Unregister sensor listener when the service is destroyed
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(this);
+            System.out.println("Accelerometer sensor unregistered");
+        }
+
+        try{
+            this.bw.close();
+        } catch (IOException e) {
+            Log.e("AccelerometerLogging", "Error closing file");
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            String[] data = { Float.toString(x), Float.toString(y), Float.toString(z)};
+            try {
+                this.bw.write(Arrays.toString(data));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Accelerometer data written to file");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used in this example
+    }
+}
---
+++ app/src/main/java/com/upside/accelerometerlogger/MainActivity.java
@@ -0,0 +1,56 @@
+package com.upside.accelerometerlogger;
+/*
+* TODO: add popup when push buttons
+* TODO: try to log with closed app
+*
+* */
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        // assigning ID of startButton
+        // to the object start
+        start = (Button) findViewById( R.id.startButton );
+
+        // assigning ID of stopButton
+        // to the object stop
+        stop = (Button) findViewById( R.id.stopButton );
+
+        // declaring listeners for the
+        // buttons to make them respond
+        // correctly according to the process
+        start.setOnClickListener( this );
+        stop.setOnClickListener( this );
+    }
+
+    public void onClick(View view) {
+
+        // process to be performed
+        // if start button is clicked
+        if(view == start){
+
+            // starting the service
+            startService(new Intent( this, AccelerometerLoggingService.class ) );
+        }
+
+        // process to be performed
+        // if stop button is clicked
+        else if (view == stop){
+
+            // stopping the service
+            stopService(new Intent( this, AccelerometerLoggingService.class ) );
+
+        }
+    }
+}
